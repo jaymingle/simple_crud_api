a8947071479a8767b5f1769bec453b944afba17f
Jay Mingle <jkawudi@gmail.com>
2025-03-16 07:22:38 +0000

    Changes

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-console.log("test")
+import express from 'express'
+import mongoose from 'mongoose'
+
+const app = express()
+const PORT = 8080
+
+mongoose.connect('mongodb+srv://jaymingle:M6LFWkhe3S9Fe1m8@backend.0r5fr.mongodb.net/Node-API?retryWrites=true&w=majority&appName=Backend')
+    .then(() => {
+        console.log('MongoDB Connection Succesful')
+        app.listen(PORT, () => console.log("Server Running"))
+    })
+    .catch(() => console.error('MongoDB Connection Failed'))
+
+app.get('/', (req, res) => {
+    res.json({
+        message: 'Server Running'
+    })
+})
